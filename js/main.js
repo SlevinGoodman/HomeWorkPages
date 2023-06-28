8bdbdff0b2c1d54792a5c0cdbdd773bf2440cd2b
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-06-28 22:22:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(function () {
+    $(".rateYo").rateYo({
+        starWidth: "36px",
+        rating: 4.5,
+        spacing: "2px",
+        
+        });
+
+    Fancybox.bind("[data-fancybox]", {
+    });
+
+
+
+
+
+});
